0a49adda29c918e9b668503fdeba6bf450560bd0
Hok Shun Poon <hok@hokshunpoon.me>
2014-11-01 13:21:09 +0000

    Appears to work without view outlet. :S

--- CalculatorExperiments/NumberPad/MESNumberPad.h
+++ CalculatorExperiments/NumberPad/MESNumberPad.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MESXibLoadedView.h"
 
-@interface MESNumberPad : UIView
-@property (strong, nonatomic) IBOutlet MESNumberPad *view;
+@interface MESNumberPad : MESXibLoadedView
 @end
