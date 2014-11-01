f637f48c31920935e3b6d96d2d215240e560ae16
Hok Shun Poon <hok@hokshunpoon.me>
2014-11-01 13:20:19 +0000

    Add XibLoadedView class.

---
+++ CalculatorExperiments/NumberPad/MESXibLoadedView.m
@@ -0,0 +1,27 @@
+//
+// Created by Hok Shun Poon on 01/11/2014.
+// Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#import "View+MASAdditions.h"
+#import "MESNumberPad.h"
+#import "MESXibLoadedView.h"
+
+
+@implementation MESXibLoadedView
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if(self) {
+        [[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class]) owner:self options:nil];
+
+        [self addSubview:self.view];
+
+        [self.view mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(self);
+        }];
+    }
+    return self;
+}
+
+@end
