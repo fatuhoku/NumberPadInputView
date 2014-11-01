46c905326feca745e6e9f731d2024d894e053ff4
Hok Shun Poon <hok@hokshunpoon.me>
2014-11-01 13:53:58 +0000

    Move everything and introduce the Calculator style number input.

---
+++ CalculatorExperiments/CalculatorStyleNumberInput/MESCalculatorStyleNumberInput.h
@@ -0,0 +1,13 @@
+//
+//  MESCalculatorStyleNumberInput.h
+//  CalculatorExperiments
+//
+//  Created by Hok Shun Poon on 01/11/2014.
+//  Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MESXibLoadedView.h"
+
+@interface MESCalculatorStyleNumberInput : MESXibLoadedView
+@end
---
+++ CalculatorExperiments/CalculatorStyleNumberInput/MESCalculatorStyleNumberInput.m
@@ -0,0 +1,12 @@
+//
+//  MESCalculatorStyleNumberInput.m
+//  CalculatorExperiments
+//
+//  Created by Hok Shun Poon on 01/11/2014.
+//  Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#import "MESCalculatorStyleNumberInput.h"
+
+@implementation MESCalculatorStyleNumberInput
+@end
