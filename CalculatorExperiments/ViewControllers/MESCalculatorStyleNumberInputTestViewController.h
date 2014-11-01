aedc0b2e3a45f4e5cdb3e332aaa6462ac348ad8e
Hok Shun Poon <hok@hokshunpoon.me>
2014-11-01 15:00:05 +0000

    Make a little test widget to go on the test ViewController so that I can control the look and feel of the widget.

---
+++ CalculatorExperiments/ViewControllers/MESCalculatorStyleNumberInputTestViewController.h
@@ -0,0 +1,11 @@
+//
+// Created by Hok Shun Poon on 01/11/2014.
+// Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface MESCalculatorStyleNumberInputTestViewController : UIViewController
+@end
---
+++ CalculatorExperiments/ViewControllers/MESCalculatorStyleNumberInputTestViewController.m
@@ -0,0 +1,25 @@
+//
+// Created by Hok Shun Poon on 01/11/2014.
+// Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import <ReactiveCocoa/UITextField+RACSignalSupport.h>
+#import "MESCalculatorStyleNumberInputTestViewController.h"
+#import "MESCalculatorStyleNumberInput.h"
+
+@interface MESCalculatorStyleNumberInputTestViewController ()
+@property(strong, nonatomic) IBOutlet MESCalculatorStyleNumberInput *calculatorStyleNumberInput;
+@property(strong, nonatomic) IBOutlet UITextField *unitField;
+@end
+
+@implementation MESCalculatorStyleNumberInputTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // RAC Bindings
+    RAC(self, calculatorStyleNumberInput.unit) = [self.unitField rac_textSignal];
+}
+
+@end
