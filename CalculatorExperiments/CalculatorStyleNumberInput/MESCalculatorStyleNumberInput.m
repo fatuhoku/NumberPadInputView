0f4401d371df59d7b220efe6010f47e00e5c00c4
Hok Shun Poon <hok@hokshunpoon.me>
2014-11-01 14:15:28 +0000

    Add outlets; bind number to display.

--- CalculatorExperiments/CalculatorStyleNumberInput/MESCalculatorStyleNumberInput.m
+++ CalculatorExperiments/CalculatorStyleNumberInput/MESCalculatorStyleNumberInput.m
@@ -6,7 +6,15 @@
 //  Copyright (c) 2014 Hok Shun Poon. All rights reserved.
 //
 
+#import <ReactiveCocoa/ReactiveCocoa.h>
 #import "MESCalculatorStyleNumberInput.h"
+#import "MESNumberDisplay.h"
+#import "MESNumberPad.h"
+
+@interface MESCalculatorStyleNumberInput ()
+@property (strong, nonatomic) IBOutlet MESNumberDisplay *numberDisplay;
+@property (strong, nonatomic) IBOutlet MESNumberPad *numberPad;
+@end
 
 @implementation MESCalculatorStyleNumberInput
 
@@ -14,6 +22,9 @@
     [super awakeFromNib];
 
     self.number = [NSDecimalNumber zero];
+
+    // RAC Bindings
+    RAC(self, numberDisplay.number) = RACObserve(self, number);
 }
 
 @end
--- CalculatorExperiments/CalculatorStyleNumberInput/NumberDisplay/MESNumberDisplay.h
+++ CalculatorExperiments/CalculatorStyleNumberInput/NumberDisplay/MESNumberDisplay.h
@@ -10,4 +10,5 @@
 #import "MESXibLoadedView.h"
 
 @interface MESNumberDisplay : MESXibLoadedView
+@property(nonatomic, strong) NSDecimalNumber *number;
 @end
