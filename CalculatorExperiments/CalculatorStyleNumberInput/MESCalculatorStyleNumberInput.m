257c94ca3447833eebefad92c7ab2773cb99883d
Hok Shun Poon <hok@hokshunpoon.me>
2014-11-01 14:38:40 +0000

    Add comment.

--- CalculatorExperiments/CalculatorStyleNumberInput/MESCalculatorStyleNumberInput.m
+++ CalculatorExperiments/CalculatorStyleNumberInput/MESCalculatorStyleNumberInput.m
@@ -32,7 +32,20 @@
         NSLog(@"Touched DP.");
     }];
 
-        // RAC Bindings
+    // 1. When a digit comes in
+    // 1.1. If the current string is 0, then replace the 0 with the new digit.
+    // 1.2. Otherwise append the new string to the end of the string.
+    // 1.3. If the number exceeds 8 digits, don't do anything.
+    //
+    // 2. When decimal point comes in
+    // 2.1 If the current string doesn't contain the decimal point then is added.
+    // 2.2 If the current string contains the decimal point already then do nothing.
+    //
+    // 3. When clear, the string is reset to "0".
+    //
+    // 4. Whenever the string is updated, exposed NSDecimalNumber.
+
+    // RAC Bindings
     RAC(self, numberDisplay.number) = RACObserve(self, number);
     RAC(self, numberDisplay.unit) = RACObserve(self, unit);
 }
